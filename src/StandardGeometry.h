6e94d8ec72e59541d7615e6973dccca2558114d3
will <will3.git@gmail.com>
2018-04-02 16:56:44 +1200

    MINOR

---
+++ src/StandardGeometry.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Geometry.h"
+
+class StandardGeometry : public Geometry {
+	void load() {
+
+	};
+
+	void unload() {
+
+	};
+
+	void bind() {
+
+	};
+
+	void unbind() {
+
+	};
+};
